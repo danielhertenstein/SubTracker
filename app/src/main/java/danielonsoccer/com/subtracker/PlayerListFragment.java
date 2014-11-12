74cfe4bd011599fb505c1cf60ce398a8e11f5694
Daniel Hertenstein <daniel.hertenstein@gmail.com>
2014-11-11 23:05:48 -0500

    Adds dialog fragment for player list.
    Adds the initial fragment to be used to display the player list for
    picking which player to substitute in.

---
+++ app/src/main/java/danielonsoccer/com/subtracker/PlayerListFragment.java
@@ -0,0 +1,31 @@
+package danielonsoccer.com.subtracker;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+/**
+ * Created by daniel on 11-Nov-14.
+ */
+public class PlayerListFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("test")
+                .setPositiveButton("positive", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Do something?
+                    }
+                })
+                .setNegativeButton("negative", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Do something?
+                    }
+                });
+        return builder.create();
+    }
+}
