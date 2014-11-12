67153486c1677f0df8d68b8c5f6c6d851e612487
Daniel Hertenstein <daniel.hertenstein@gmail.com>
2014-11-12 11:30:34 -0500

    Adds title to PlayerListFragment

--- app/src/main/java/danielonsoccer/com/subtracker/PlayerListFragment.java
+++ app/src/main/java/danielonsoccer/com/subtracker/PlayerListFragment.java
@@ -13,7 +13,8 @@ public class PlayerListFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setMessage("test")
+        builder.setTitle(R.string.player_list)
+                .setMessage("test")
                 .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
