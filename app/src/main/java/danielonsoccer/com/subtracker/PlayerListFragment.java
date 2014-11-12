b8b67ef4749a986cf88516b59520d2c762d80eb2
Daniel Hertenstein <daniel.hertenstein@gmail.com>
2014-11-12 16:10:25 -0500

    Adds public interface to PlayerListFragment.
    This will allow the fragment to pass events back to the main activity.
    Hopefully I can use this to switch the names on the button.

--- app/src/main/java/danielonsoccer/com/subtracker/PlayerListFragment.java
+++ app/src/main/java/danielonsoccer/com/subtracker/PlayerListFragment.java
@@ -1,5 +1,6 @@
 package danielonsoccer.com.subtracker;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
@@ -10,6 +11,22 @@ import android.os.Bundle;
  * Created by daniel on 11-Nov-14.
  */
 public class PlayerListFragment extends DialogFragment {
+    public interface PlayerListListener {
+        public void onDialogItemClick(DialogFragment dialog);
+        public void onDialogNegativeClick(DialogFragment dialog);
+    }
+    PlayerListListener listener;
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            listener = (PlayerListListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString() + " must implement PlayerListListener");
+        }
+    }
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
@@ -17,13 +34,13 @@ public class PlayerListFragment extends DialogFragment {
                 .setItems(R.array.player_list_array, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        // Substitute the player
+                        listener.onDialogItemClick(PlayerListFragment.this);
                     }
                 })
                 .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        // Do something?
+                        listener.onDialogNegativeClick(PlayerListFragment.this);
                     }
                 });
         return builder.create();
