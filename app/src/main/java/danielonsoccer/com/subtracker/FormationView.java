2449615d0189ff38454492ca5bdfc74c95cead60
Daniel Hertenstein <daniel.hertenstein@gmail.com>
2014-12-16 19:14:43 -0500

    Adds first try at the animation after players are clicked.
    I think I'm going to transition to creating a new card when players are
    clicked. I assume that will make it easier to do the color changing.

--- app/src/main/java/danielonsoccer/com/subtracker/FormationView.java
+++ app/src/main/java/danielonsoccer/com/subtracker/FormationView.java
@@ -1,15 +1,16 @@
 package danielonsoccer.com.subtracker;
 
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.SystemClock;
 import android.os.Vibrator;
+import android.support.v7.widget.CardView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.TextView;
 
@@ -26,11 +27,19 @@ public class FormationView extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set the layout.
         setContentView(R.layout.activity_formation_view);
+
+        // Start the player timer.
         playerDuration = (Chronometer) findViewById(R.id.lf_chronometer);
         playerDuration.start();
+
+        // Starts the substitution countdown timer.
         substitutionTimerTextView = (TextView) findViewById(R.id.substitution_interval_timer);
         substitutionTimer = new CountDownTimer(300000, 1000) {
+
+            // Display the time remaining on every tick in MM:SS format.
             public void onTick(long millisUntilFinished) {
                 substitutionTimerTextView.setText(String.format("%02d:%02d",
                         TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished),
@@ -39,6 +48,8 @@ public class FormationView extends Activity {
                                         TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))));
             }
 
+            // Vibrate the phone three times when the timer expires to indicate that substitutions
+            // should be executed.
             public void onFinish() {
                 Vibrator vibrator = (Vibrator) getApplicationContext().getSystemService(Context
                         .VIBRATOR_SERVICE);
@@ -52,19 +63,18 @@ public class FormationView extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+
+        // Inflate the menu.
         getMenuInflater().inflate(R.menu.menu_formation_view, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+        // Handle action bar item clicks.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
+        // Open the settings.
         if (id == R.id.action_settings) {
             return true;
         }
@@ -72,14 +82,30 @@ public class FormationView extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    // Method called when a player is clicked.
+    // Opens the dialogue to pick a substitute.
     public void openSubList(View view) {
         PlayerListFragment dialog = PlayerListFragment.newInstance(R.array.player_list_array);
         dialog.show(getFragmentManager(), "PlayerListFragment");
     }
 
+    // Method called when the "execute substitutions" button is clicked.
+    public void resetCard(View view) {
+        CardView cardView = (CardView) findViewById(R.id.lf_card);
+        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(cardView, "scaleY", 1.0f);
+        objectAnimator.setDuration(1000);
+        objectAnimator.start();
+    }
+
+    // Code executed after a substitute is queued.
     public void doItemClick(String playerName) {
         TextView textView = (TextView) findViewById(R.id.current_lf);
         textView.setText(playerName);
+        CardView cardView = (CardView) findViewById(R.id.lf_card);
+        cardView.setPivotY(100);
+        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(cardView, "scaleY", 2.0f);
+        objectAnimator.setDuration(1000);
+        objectAnimator.start();
         // Substitute player
         playerDuration.setBase(SystemClock.elapsedRealtime());
     }
